54510376984ceba8901637b98f48beef6fd99f9d
GitHub Action <action@github.com>
2020-08-20 03:20:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/vladyslavvolkov/jetbrainsideplugindigitalocean/MyBundle.kt
+++ src/main/kotlin/com/github/vladyslavvolkov/jetbrainsideplugindigitalocean/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vladyslavvolkov.jetbrainsideplugindigitalocean
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/vladyslavvolkov/jetbrainsideplugindigitalocean/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vladyslavvolkov.jetbrainsideplugindigitalocean.services
+
+import com.github.vladyslavvolkov.jetbrainsideplugindigitalocean.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
